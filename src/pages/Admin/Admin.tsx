6a37818abf5ddaa7e34342c8e8d93d1b462a4922
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-09 02:47:35 +0500

    added bread crumbs to admin dashboard

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -9,29 +9,53 @@ import { Container } from "@/components/helpers/Container";
 import { Footer } from "@/components/layout/Footer";
 
 export const Admin = () => {
-  const { isAdmin } = useAuth();
+  const { isAdmin, admin } = useAuth();
   const navigate = useNavigate();
-  const [admin, setAdmin] = useState(false);
+  const [isIAdmin, setIsIAdmin] = useState(false);
 
   isAdmin()
     .then((res) => {
       console.log(res);
-      setAdmin(true);
+      setIsIAdmin(true);
     })
     .catch((e) => {
       console.error(e);
-      setAdmin(false);
+      setIsIAdmin(false);
       navigate("/admin/login");
     });
 
+  const paths = location.pathname.split("/");
+
   return (
-    admin && (
+    isIAdmin && (
       <>
         <Header />
         <main>
           <section>
             <Container>
-              <h1 className="text-2xl">Welcome Admin</h1>
+              <div className="flex w-full justify-between items-center">
+                <div>
+                  {paths.map((path, index) => {
+                    const fullPath = paths.slice(0, index + 1).join("/");
+                    return (
+                      <span key={index} className="last:text-ex-black">
+                        <a
+                          href={location.origin + `${fullPath}`}
+                          className="hover:underline"
+                        >
+                          {path || "Home"}
+                        </a>
+                        {index < paths.length - 1 && " / "}
+                      </span>
+                    );
+                  })}
+                </div>
+                <p>
+                  Welcome!{" "}
+                  <span className="text-ex-red">{admin.firstName}</span>
+                </p>
+              </div>
+              <h1 className="text-[40px] font-medium my-20">Admin panel</h1>
             </Container>
           </section>
         </main>
